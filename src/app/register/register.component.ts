6c24ae591a5cfab3a1f6c46d27d34a6f68d320ee
as-dev <tst.1a2a3@gmail.com>
2024-10-06 15:20:26 +0200

    complete register page

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService
+      .register(
+        this.registerForm.value.firstName!,
+        this.registerForm.value.lastName!,
+        this.registerForm.value.email!,
+        this.registerForm.value.password!
+      )
+      .subscribe(() => {
+        this.router.navigateByUrl('/').then(() => {
+          window.location.reload();
+        });
+      });
+  }
+}
